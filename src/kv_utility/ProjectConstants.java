7f38a70833e7e6119003b75849b5bb049b3f5fd9
Glen Menezes <gmeneze@ncsu.edu>
2016-11-26 18:25:25 -0500

    Name Update

--- src/kv_utility/ProjectConstants.java
+++ src/kv_utility/ProjectConstants.java
@@ -4,4 +4,12 @@ package kv_utility;
  * Created by abhishek on 11/26/16.
  */
 public class ProjectConstants {
+    /* PORT CONSTANTS */
+    public final static int REQUEST_COORDINATOR_LISTEN_PORT = 7091;
+
+
+    /*MISC CONSTANTS */
+    public final static int MAX_CLIENT_REQUEST_BACKLOG_SIZE = 25;
+    public final static int NUM_OF_WORKERS = 10;
+    public final static int ONE = 1;
 }

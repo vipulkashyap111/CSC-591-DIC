b51053dd592a27df7878f4657fe3a233673f82c1
Glen Menezes <gmeneze@ncsu.edu>
2016-11-27 01:37:51 -0500

    Added Data Structure Classes

--- src/kv_utility/ClientResponsePacket.java
+++ src/kv_utility/ClientResponsePacket.java
@@ -9,4 +9,44 @@ public class ClientResponsePacket {
     private String key;
     private String value;
     private String response_code;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getReturn_status() {
+        return return_status;
+    }
+
+    public void setReturn_status(int return_status) {
+        this.return_status = return_status;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getResponse_code() {
+        return response_code;
+    }
+
+    public void setResponse_code(String response_code) {
+        this.response_code = response_code;
+    }
 }

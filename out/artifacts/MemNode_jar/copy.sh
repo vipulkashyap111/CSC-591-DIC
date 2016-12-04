b8bea4849753ca6757c704f61ddaf985abab445c
abhishek <mail.abhisheklingwal@gmail.com>
2016-12-03 19:32:52 -0500

    Added new filed in valudetail

---
+++ out/artifacts/MemNode_jar/copy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+scp -i ~/Downloads/DIC.pem MemNodeProc.jar ubuntu@54.161.217.151:~/
+scp -i ~/Downloads/DIC.pem MemNodeProc.jar ubuntu@54.87.131.221:~/
+scp -i ~/Downloads/DIC.pem MemNodeProc.jar ubuntu@54.174.115.3:~/

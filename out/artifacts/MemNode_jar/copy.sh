ac4b709acb552489bc228c42d4f1df8a1150dd83
Vipul Kashyap <vipulkashyap@Vipuls-MacBook-Pro.local>
2016-12-04 22:25:37 -0500

    Removed unncessary files

--- out/artifacts/MemNode_jar/copy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-scp -i ~/Downloads/DIC.pem MemNodeProc.jar ubuntu@54.161.217.151:~/
-scp -i ~/Downloads/DIC.pem MemNodeProc.jar ubuntu@54.87.131.221:~/
-scp -i ~/Downloads/DIC.pem MemNodeProc.jar ubuntu@54.174.115.3:~/
-scp -i ~/Downloads/DIC.pem MemNodeProc.jar ubuntu@54.166.11.199:~/
-scp -i ~/Downloads/DIC.pem MemNodeProc.jar ubuntu@54.166.27.87:~/
